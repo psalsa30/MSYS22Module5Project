b3a7cdf2ae2741b0ebaf1af253e1385c7a49781a
psalsa30 <pauloualopsalvador@gmail.com>
2026-03-16 21:35:10 +0800

    urls update

--- M5P/MyInventoryApp/urls.py
+++ M5P/MyInventoryApp/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.view_supplier, name='view_supplier'),
     path('view_supplier', views.view_supplier, name='view_supplier'),
     path('view_bottles', views.view_bottles, name='view_bottles'),
     path('add_bottle', views.add_bottle, name='add_bottle'),
